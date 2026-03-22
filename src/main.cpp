f0c52b8420882c171a7ad9601dbc9d07acd0fa1f
Mahoskian <sohamnaik15@gmail.com>
2026-03-22 18:00:58 -0400

    Added deps

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "stb_image_resize2.h"
+
+#include <filesystem>
+#include <bitset>
+#include <thread>
+#include <execution>
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!";
+  return 0;
+}
